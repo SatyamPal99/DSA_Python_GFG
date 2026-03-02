80f98a0db80e2b53a72d4acceb4f8143a7355f0e
Satyam Pal <Satyampal243ss@gmail.com>
2026-03-02 19:18:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Head of Linked List/delete-head-of-linked-list.py
+++ Difficulty: Easy/Delete Head of Linked List/delete-head-of-linked-list.py
@@ -4,9 +4,11 @@ class Node:
         self.data = data
         self.next = None
 '''
-
+import sys
 class Solution:
     def deleteHead(self, head):
+        if head==None:
+            return None
         temp=head
         head=head.next
         temp.next=None
