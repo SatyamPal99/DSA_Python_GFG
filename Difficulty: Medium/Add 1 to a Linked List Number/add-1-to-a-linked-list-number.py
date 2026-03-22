4c8970d3ff14edc360ea28b4760ef7c0bb604c2b
Satyam Pal <Satyampal243ss@gmail.com>
2026-03-22 23:32:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.py
@@ -0,0 +1,47 @@
+'''
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def addOne(self,head):
+        head1=self.reverse(head)
+        carry=1
+        curr=head1
+        while curr:
+            val=curr.data+carry
+            if val<10:
+                curr.data=val
+                carry=0
+                break
+            else:
+                carry=1
+                curr.data=0
+            curr=curr.next
+        
+        if carry==1:
+            new=Node(1)
+            head1=self.reverse(head1)
+            new.next=head1
+            return new
+        head1=self.reverse(head1)
+        return head1
+                
+        
+        
+        
+    def reverse(self,head):
+        temp=head
+        pre=None
+        curr=head
+        while temp:
+            curr=curr.next
+            temp.next=pre
+            pre=temp
+            temp=curr
+        return pre
+            
+            
