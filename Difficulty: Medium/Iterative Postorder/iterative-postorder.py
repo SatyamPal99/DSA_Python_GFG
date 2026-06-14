4b1f2134fc9d8ebc56510edbc1202320064e081a
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-14 17:25:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Postorder/iterative-postorder.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+'''
+# Node Class:
+class Node:
+    def init(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    # Return a list containing the post order traversal of the given tree
+    def postOrder(self,node):
+        # Using two stack
+        st1=[node]
+        st2=[]
+        while st1:
+            temp=st1.pop()
+            st2.append(temp.data)
+            if temp.left!=None:
+                st1.append(temp.left)
+            if temp.right!=None:
+                st1.append(temp.right)
+        return st2[::-1]
