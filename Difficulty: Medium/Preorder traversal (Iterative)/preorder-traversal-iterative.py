696ead7c5347aa976e2eeb8ab36adf5bee7a31f8
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-14 11:49:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.py
@@ -0,0 +1,22 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def preOrder(self, root):
+        st=[]
+        st.append(root)
+        ans=[]
+        while st:
+            temp=st.pop()
+            ans.append(temp.data)
+            if temp.right!=None:
+                st.append(temp.right)
+            if temp.left!=None:
+                st.append(temp.left)
+        return ans
+            
