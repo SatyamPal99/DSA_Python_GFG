7ac611eed62ac0db150494cac233b44b0b49db8f
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-15 13:13:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Iterative Inorder/iterative-inorder.py
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.py
@@ -7,7 +7,7 @@
 
 class Solution:
     def inOrder(self, root):
-        st=[]
+        """st=[]
         ans=[]
         temp=root
         while True:
@@ -20,5 +20,34 @@ class Solution:
                 temp=st.pop()
                 ans.append(temp.data)
                 temp=temp.right
+        return ans"""
+        
+        # Method2 using visited array...
+        
+        st=[root]
+        visited=[0]
+        ans=[]
+        while st:
+            temp=st.pop()
+            flag=visited.pop()
+            if not flag:
+                if temp.right!=None:
+                    st.append(temp.right)
+                    visited.append(0)
+                st.append(temp)
+                visited.append(1)
+                if temp.left!=None:
+                    st.append(temp.left)
+                    visited.append(0)
+            else:
+                ans.append(temp.data)
         return ans
+            
+            
+            
+        
+        
+        
+        
+        
         
