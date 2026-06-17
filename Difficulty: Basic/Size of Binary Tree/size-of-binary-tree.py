838e22997b293711547bfb54500a20133f9c442f
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-17 18:31:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.py
@@ -0,0 +1,16 @@
+"""
+Definition for Node
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+"""
+class Solution:
+    def getSize(self, root):
+        if root==None:
+            return 0
+        left=self.getSize(root.left)
+        right=self.getSize(root.right)
+        return left+right+1
+        
