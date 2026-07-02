97aff5c09eab596f78185b7bdd94f823e975d237
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-02 18:02:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BST Insert/bst-insert.py
+++ Difficulty: Easy/BST Insert/bst-insert.py
@@ -9,10 +9,35 @@ class Node:
 
 class Solution:
     def insert(self, root, key):
-        if root==None:
+        # Recursive way
+        """if root==None:
             return Node(key)
         if key<root.data:
             root.left=self.insert(root.left,key)
         if key>root.data:
             root.right=self.insert(root.right,key)
-        return root
+        return root"""
+        
+        # Iterative way
+        
+        if root==None:
+            return Node(key,None,None)
+        curr=root
+        while curr:
+            if curr.data>key:
+                if curr.left==None:
+                    new=Node(key)
+                    curr.left=new
+                    return root
+                curr=curr.left
+            elif curr.data<key:
+                if curr.right==None:
+                    new=Node(key)
+                    curr.right=new
+                    return root
+                curr=curr.right
+                
+        
+        
+        
+        
