730cde45017c89732f2b96cd2bc620c699facec6
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-02 15:46:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Preorder Traversal/preorder-traversal.py
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.py
@@ -9,9 +9,37 @@ class Node:
 
 class Solution:
     def preOrder(self, root):
-        ans=[]
+        """ans=[]
         self.fun(root,ans)
+        return ans"""
+        
+        curr=root
+        ans=[]
+        while curr:
+            if curr.left==None:
+                ans.append(curr.data)
+                curr=curr.right
+            else:
+                predecessor=self.find_pred(curr)
+                if predecessor.right==None:
+                    ans.append(curr.data)
+                    predecessor.right=curr
+                    curr=curr.left
+                else:
+                    predecessor.right=None
+                    curr=curr.right
         return ans
+                    
+                
+                
+    def find_pred(self,root):
+        pred=root.left
+        while pred.right and pred.right!=root:
+            pred=pred.right
+        return pred
+        
+        
+        
     def fun(self,root,ans):
         if root==None:
             return 
