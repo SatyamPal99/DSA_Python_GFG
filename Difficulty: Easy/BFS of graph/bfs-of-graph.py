41bd3b8e2992d04a32b58530d2b4300c5728bf43
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-16 04:16:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.py
@@ -0,0 +1,17 @@
+class Solution:
+    def bfs(self, adj):
+        n=len(adj)
+        vis=[0]*n
+        q=deque()
+        q.append(0)
+        vis[0]=1
+        ans=[]
+        while q:
+            node=q.popleft()
+            ans.append(node)
+            for i in adj[node]:
+                if vis[i]==0:
+                    q.append(i)
+                    vis[i]=1
+        return ans
+        
