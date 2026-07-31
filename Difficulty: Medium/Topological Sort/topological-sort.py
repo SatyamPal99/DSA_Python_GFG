2524bcc9af2e0d99e63076af431d42cbd1ef4349
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-31 16:15:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.py
+++ Difficulty: Medium/Topological Sort/topological-sort.py
@@ -1,6 +1,34 @@
+from collections import deque
 class Solution:
     def topoSort(self, V, edges):
-        vis=[0]*V
+        # Usinf BFS(Kahn's Algorithm)
+        adj=[[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
+        INdegree=[0]*V
+        for i in range(V):
+            for j in adj[i]:
+                INdegree[j]+=1
+        q=deque()
+        for i in range(V):
+            if INdegree[i]==0:
+                q.append(i)
+        ans=[]
+        while q:
+            node=q.popleft()
+            ans.append(node)
+            for i in adj[node]:
+                INdegree[i]-=1
+                if INdegree[i]==0:
+                    q.append(i)
+        return ans
+        
+        
+        
+        
+        
+        
+        """vis=[0]*V
         adj=[[] for _ in range(V)]
         for u,v in edges:
             adj[u].append(v)
@@ -19,6 +47,6 @@ class Solution:
         for i in adj[node]:
             if vis[i]==0:
                 self.dfs(vis,adj,i,st)
-        st.append(node)
+        st.append(node)"""
                 
         
