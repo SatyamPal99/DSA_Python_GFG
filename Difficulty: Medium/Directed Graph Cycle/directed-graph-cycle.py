7b7664f3a1541c7e92b0d062328778a336992344
Satyam Pal <Satyampal243ss@gmail.com>
2026-08-01 17:14:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
@@ -1,6 +1,7 @@
 class Solution:
     def isCyclic(self, V, edges):
-        vis=[0]*V
+        # using dfs
+        """vis=[0]*V
         pathvis=[0]*V
         adj=[[] for _ in range(V)]
         for u,v in edges:
@@ -10,7 +11,33 @@ class Solution:
             if vis[i]==0:
                 if self.dfs(adj,i,vis,pathvis)==True:
                     return True
+        return False"""
+        
+        vis=[0]*V
+        adj=[[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
+        inDeg=[0]*V
+        q=deque()
+        for i in range(V):
+            for j in adj[i]:
+                inDeg[j]+=1
+        for j in range(V):
+            if inDeg[j]==0:
+                q.append(j)
+        count=0
+        while q:
+            node=q.popleft()
+            count+=1
+            for i in adj[node]:
+                inDeg[i]-=1
+                if inDeg[i]==0:
+                    q.append(i)
+        if count<V:
+            return True
         return False
+    
+        
                 
                 
     def dfs(self,adj,node,vis,pathvis):
@@ -24,6 +51,14 @@ class Solution:
                 return True
         pathvis[node]=0
         return False
+    
+    
+    
+                
+                
+    
+    
+    
         
         
         
