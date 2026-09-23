144c420816ff05c7a0f7c7a5041882e46fb8a884
Satyam Pal <Satyampal243ss@gmail.com>
2026-09-24 00:01:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.py
+++ Difficulty: Medium/Geek's Training/geeks-training.py
@@ -1,8 +1,27 @@
 class Solution:
     def maximumPoints(self, mat):
         n=len(mat)
+        """dp=[[-1]*4 for _ in range(n)]
+        return self.fun(n-1,3,mat,dp)"""
+        
+        # Tabulation (bottom-up)
+        
         dp=[[-1]*4 for _ in range(n)]
-        return self.fun(n-1,3,mat,dp)
+        dp[0][0]=max(mat[0][1],mat[0][2])
+        dp[0][1]=max(mat[0][0],mat[0][2])
+        dp[0][2]=max(mat[0][0],mat[0][1])
+        dp[0][3]=max(mat[0][1],max(mat[0][1],mat[0][2]))
+        
+        for day in range(1,n):
+            for last in range(4):
+                dp[day][last]=0
+                for task in range(3):
+                    if task!=last:
+                        score=mat[day][task]+dp[day-1][task]
+                        dp[day][last]=max(dp[day][last],score)
+        return dp[n-1][3]
+                
+        
         
     def fun(self,n,last,mat,dp):
         if n==0:
